36c71b8d6cd152dcd7d0ccced918c49d781f7f71
Taynan Breno <thaynanbreno@gmail.com>
2024-06-11 14:43:37 -0300

    feat: connection database with mongoose

---
+++ src/database/index.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+export class DbConnection {
+	async connect() {
+		try {
+			await mongoose.connect(process.env.DATABASE_URL!)
+			console.log('Database connected')
+		} catch (error) {
+			console.error('Error to connect database')
+		}
+	}
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
-import express, { NextFunction, Request, Response } from 'express'
-import { UserRoutes } from './routes/user.routes'
+import express, { Application, NextFunction, Request, Response } from 'express'
+import dotenv from 'dotenv'
 
-const app = express()
+import { UserRoutes } from './routes/user.routes'
+import { DbConnection } from './database'
+
+dotenv.config()
+
+const app: Application = express()
 const userRoutes = new UserRoutes().getRoutes()
+const database = new DbConnection()
 const port = 3333
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+database.connect()
 app.use('/', userRoutes)
 app.use(
 	(error: Error, request: Request, response: Response, next: NextFunction) => {
